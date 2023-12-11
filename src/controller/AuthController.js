5ffcf4821a2bb0ab52557a79a5fb9d9abb16cbd8
Fabricio Taveira Carvalho Marques <tr26330es@trf1.gov.br>
2023-12-11 09:52:09 -0300

    Starting the authenticate with jwt

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require('../config/auth.json');
 
 const UserModel = require('../models/User');
 
@@ -13,7 +16,7 @@ router.post('/register', async (req,res) =>{
             error: true,
             message: "Email already registered"
         })
-    };
+    }
 
     const User = await UserModel.create(req.body);
 
@@ -23,7 +26,40 @@ router.post('/register', async (req,res) =>{
         error: false,
         message: "Registered sucefull!",
         data: User
+    })
+})
+
+router.post("/authenticate", async(req, res)=>{
+    const {email, password} = req.body;
+
+    const user = await UserModel.findOne({email}).select("password");
+    console.log(user);
+
+    if(!user){
+        return res.status(400).json({
+            error:true,
+            message: 'User not found'
+        })
+    }
+
+    if(!await bcryptjs.compare(password, user.password)){
+        return res.status(400).send({
+            error: true,
+            message:'Invalid password'
+        })
+    }
+
+    user.password = undefined;
+
+    const token = jwt.sign({
+        id: user.id,
+        name: user.name
+    }, authConfig.secret ,{});
+
+    return res.json({
+        user,
+        token
     });
-});
+})
 
 module.exports = router;
